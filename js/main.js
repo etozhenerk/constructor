8e438b924d6413d6452c19b5ba30e84075700f5d
etozhenerk <etozhenerk@yandex.ru>
2021-07-22 00:41:38 +0300

    createMain

--- js/main.js
+++ js/main.js
@@ -52,6 +52,21 @@ const createHeader = (param) => {
     wrapper.append(logo);
   }
 
+  if (param.header.menu) {
+    const nav = getElement("nav", ["menu-list"]);
+    const allMenuLink = param.header.menu.map((item) => {
+      const link = getElement("a", ["menu-link"], {
+        href: item.link,
+        textContent: item.title,
+      });
+
+      return link;
+    });
+
+    nav.append(...allMenuLink);
+    wrapper.append(nav);
+  }
+
   if (param.header.social) {
     const socialWrapper = getElement("div", ["social"]);
     const allSocial = param.header.social.map((item) => {
@@ -78,18 +93,109 @@ const createHeader = (param) => {
   return header;
 };
 
+const createMain = ({
+  title,
+  main: { genre, rating, description, trailer },
+}) => {
+  const main = getElement("main");
+  const container = getElement("div", ["container"]);
+  main.append(container);
+  const wrapper = getElement("div", ["main-content"]);
+  container.append(wrapper);
+  const content = getElement("div", ["content"]);
+  wrapper.append(content);
+
+  if (genre) {
+    const genreSpan = getElement("span", ["genre", "animated", "fadeInRight"], {
+      textContent: genre,
+    });
+
+    content.append(genreSpan);
+  }
+
+  if (rating) {
+    const ratingBlock = getElement("div", [
+      "rating",
+      "animated",
+      "fadeInRight",
+    ]);
+    const ratingStars = getElement("div", ["rating-stars"]);
+    const ratingNumber = getElement("div", ["rating-number"], {
+      textContent: `${rating}/10`,
+    });
+
+    for (let i = 0; i < 10; i++) {
+			const star = getElement('img', ['star'], {
+				alt: i ? '' : `Рейтинг ${rating} из 10`,
+				src:  i < rating ? './img/star.svg' : './img/star-o.svg',
+			});
+
+			ratingStars.append(star);
+		}
+
+    ratingBlock.append(ratingStars, ratingNumber);
+    content.append(ratingBlock);
+  }
+
+	content.append(getElement('h1', ['main-title', 'animated', 'fadeInRight'], {
+		textContent: title,
+	}));
+
+	if(description){
+		const descriptionElem = getElement('p', ['main-description', 'animated', 'fadeInRight'], {
+			textContent: description,
+		});
+
+		content.append(descriptionElem);
+	}
+
+	if(trailer){
+		const youtubeLink = getElement('a', ['button', 'animated', 'fadeInRight', 'youtube-modal'], {
+			href: trailer,
+			textContent: "Смотреть трейлер",
+		});
+
+		const youtubeImageLink = getElement('a', ['play', 'youtube-modal'], {
+			href: trailer,
+		});
+
+		const iconPlay = getElement('img', ['play-img'], {
+			src: './img/play.svg',
+			alt: 'play',
+			areaHidden: true,
+		});
+
+
+		content.append(youtubeLink);
+		wrapper.append(youtubeImageLink);
+		youtubeImageLink.append(iconPlay);
+	}
+
+  return main;
+};
+
 const movieConstructor = (selector, options) => {
   const app = document.querySelector(selector);
   app.classList.add("body-app");
 
+  app.style.backgroundImage = options.background
+    ? `url("${options.background}")`
+    : "";
+
+  document.title = options.title;
+
   if (options.header) {
-    const header = createHeader(options);
-    app.append(header);
+    app.append(createHeader(options));
+  }
+
+  if (options.main) {
+    app.append(createMain(options));
   }
 };
 
 movieConstructor(".app", {
   title: "Ведьмак",
+  background: "./witcher/background.jpg",
   header: {
     logo: "./witcher/logo.png",
     social: [
@@ -109,19 +215,26 @@ movieConstructor(".app", {
         image: "./witcher/social/facebook.svg",
       },
     ],
-		menu: [
-			{
-				title:'Описание',
-				link: '#',
-			},
-			{
-				title:'Трейлер',
-				link: '#',
-			},
-			{
-				title:'Отзывы',
-				link: '#',
-			},
-		]
+    menu: [
+      {
+        title: "Описание",
+        link: "#",
+      },
+      {
+        title: "Трейлер",
+        link: "#",
+      },
+      {
+        title: "Отзывы",
+        link: "#",
+      },
+    ],
+  },
+  main: {
+    genre: "2019, фэнтези",
+    rating: 8,
+    description:
+      "Ведьмак Геральт, мутант и убийца чудовищ, на своей верной лошади по кличке Плотва путешествует по Континенту. За тугой мешочек чеканных монет этот мужчина избавит вас от всякой настырной нечисти — хоть от чудищ болотных, оборотней и даже заколдованных принцесс.",
+    trailer: "https://www.youtube.com/watch?v=P0oJqfLzZzQ",
   },
 });
